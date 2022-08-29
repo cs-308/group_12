12f6fb72bb6fa78ea4eb0723530c2f40c17cebf5
gd-codes <45459312+gd-codes@users.noreply.github.com>
2022-08-29 15:42:32 +0530

    commit 1

---
+++ factorial.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "functions.h"
+
+int factorial(int n){
+    if (n<0) {
+        std::cout << "Please enter a positive number";
+    }
+    if(n!=1){
+	return(n * factorial(n-1));
+    }
+    else return 1;
+}
